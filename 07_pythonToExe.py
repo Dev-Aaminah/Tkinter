be3fc75b815a3f1cd55e1a7bb6108e59ef0b3b05
Dev-Aaminah <aminakanwal12345@gmail.com>
2024-05-19 21:45:35 +0500

    Python to .exe file

---
+++ 07_pythonToExe.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+root = Tk()
+root.geometry("300x300+150+150")
+lbl = Label(root,bg="teal", text="Aaminah :)").pack()
+btn = Button(root, text="Click Me").pack(side=BOTTOM)
+
+root.mainloop()
