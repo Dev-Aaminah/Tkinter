0b3136958a8fa3514cdf9f8fbbdc351b3813e2f1
Dev-Aaminah <aminakanwal12345@gmail.com>
2024-05-19 13:37:24 +0500

    first code file of tkinter

---
+++ 01.py
@@ -0,0 +1,14 @@
+# import module
+from tkinter import *
+
+# creating window
+
+root  = Tk()
+
+# label
+theLabel = Label(root, text= "Hello")
+# to print label on window
+theLabel.pack()
+
+# keep showing window until user hits close icon
+root.mainloop()https://github.com/Dev-Aaminah/Tkinter
