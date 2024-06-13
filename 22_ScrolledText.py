f42a1fb2a000b64f54904f5b6ff9f426187dd065
Dev-Aaminah <aminakanwal12345@gmail.com>
2024-06-13 20:52:34 +0500

    Added Tkinter window with scrolled text widget

--- 22_ScrolledText.py
+++ 22_ScrolledText.py
@@ -1,16 +1,18 @@
-import tkinter as tk
-from tkinter import ttk
-import ttkbootstrap
-from tkinter import scrolledtext
+import tkinter as tk  # Import tkinter as tk
+from tkinter import ttk  # Import ttk from tkinter
+import ttkbootstrap  # Import ttkbootstrap
+from tkinter import scrolledtext  # Import scrolledtext from tkinter
 
+# Initialize the main window using tkinter
 Window = tk.Tk()
-Window.title('Scrolled Text')
-Window.geometry('800x550')
+Window.title('Scrolled Text')  # Set the title of the window
+Window.geometry('800x550')  # Set the size of the window
 
+# Create and pack a scrolled text widget
 scrolled_text = scrolledtext.ScrolledText(Window,
-    width=50,
-    height=10)
-scrolled_text.pack()
+    width=50,  # Set the width of the scrolled text widget
+    height=10)  # Set the height of the scrolled text widget
+scrolled_text.pack()  # Add the scrolled text widget to the window
 
-
+# Start the Tkinter event loop
 Window.mainloop()
